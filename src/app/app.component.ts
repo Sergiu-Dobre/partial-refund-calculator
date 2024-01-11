6a075952f453bd64bceb45d3ca5b19bb0abbfff4
serhio107 <serhio107@gmail.com>
2024-01-11 13:49:43 +0100

    bare bones
    added the formula and the basic GUI possible for the calculator

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -7,4 +7,23 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'partial-refund-calculator';
+  expression: string = '';
+  solution: number | null = null;
+  number_of_months: number | null = null;
+  sales_price: number | null = null;
+
+  clear(): void {
+    this.expression = '';
+  }
+
+  calculate(): void {
+    try {
+      const salesPrice: number = this.sales_price !== null ? parseFloat(this.sales_price.toString()) : 0;
+      const numberOfMonths: number = this.number_of_months !== null ? parseFloat(this.number_of_months.toString()) : 0;
+
+      this.solution = salesPrice - (salesPrice / 24) * numberOfMonths;
+    } catch (error) {
+      this.solution = null;
+    }
+  }
 }
